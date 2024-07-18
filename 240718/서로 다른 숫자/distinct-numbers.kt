817e7468b629724de81cd8c33eed9070f9c21b17
givemea1 <givemea4702@gmail.com>
2024-07-18 18:51:23 +0900

    Title: 서로 다른 숫자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240718/서로 다른 숫자/distinct-numbers.kt
@@ -0,0 +1,12 @@
+fun main()  {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val num = br.readLine().toInt()
+    val set: HashSet<Int> =
+        br
+            .readLine()
+            .trim()
+            .split(" ")
+            .map { it.toInt() }
+            .toHashSet()
+    println(set.size)
+}
