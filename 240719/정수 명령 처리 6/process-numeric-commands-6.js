a8493f41be82c4528c006883618e77d007cce414
givemea1 <givemea4702@gmail.com>
2024-07-19 11:12:52 +0900

    Title: 정수 명령 처리 6, Time: 354ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/정수 명령 처리 6/process-numeric-commands-6.js
+++ 240719/정수 명령 처리 6/process-numeric-commands-6.js
@@ -1,10 +1,67 @@
-const Heap = require('collections/heap');
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    swap(idx1, idx2) {
+        [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]]
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    push(val) {
+        this.heap.push(val);
+
+        let i = this.heap.length - 1;
+        let p = Math.floor((i - 1)/2);
+
+        while (this.heap[p] && this.heap[i] > this.heap[p]) {
+            this.swap(i, p);
+            i = p;
+            p = Math.floor((i - 1)/2);
+        }
+    }
+
+    pop() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+
+        let val = this.heap[0];
+
+        this.heap[0] = this.heap.pop();
+
+        let i = 0;
+        let l = i * 2 + 1;
+        let r = i * 2 + 2;
+
+        while (this.heap[l] && this.heap[l] > this.heap[i]
+        || this.heap[r] && this.heap[r] > this.heap[r]) {
+            let maxIdx = l;
+            if (this.heap[r] && this.heap[r] > this.heap[l]) {
+                maxIdx = r;
+            }
+
+            i = maxIdx;
+            l = i * 2 + 1;
+            r = i * 2 + 2;
+        }
+
+        return val;
+    }
+}
+
 const fs = require('fs');
 
 const commands = fs.readFileSync(0).toString().trim().split("\n")
 .slice(1).map(i => i.trim().split(" "));
 
-let h = new Heap();
+let h = new MaxHeap();
 
 commands.forEach(com => {
     switch(com[0]) {
@@ -15,10 +72,10 @@ commands.forEach(com => {
             console.log(h.pop());
             break;
         case "size":
-            console.log(h.length);
+            console.log(h.size());
             break;
         case "empty":
-            console.log(h.length === 0 ? 1 : 0);
+            console.log(h.size() === 0 ? 1 : 0);
             break;
         case "top":
             console.log(h.peek());
