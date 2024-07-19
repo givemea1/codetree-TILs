3f591504a211bfab34d8f19cdf509385d539a596
givemea1 <givemea4702@gmail.com>
2024-07-19 11:59:07 +0900

    Title: 연결리스트 1, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240719/연결리스트 1/linked-list1.js
@@ -0,0 +1,96 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DLL {
+    constructor(str) {
+        let node = new Node(str);
+        this.head = node;
+        this.tail = node;
+        this.cur = node;
+        this.size = 1;
+    }
+
+    addFront(node) {
+        if (this.cur.prev === null) {
+            node.next = this.cur;
+            this.cur.prev = node;
+            this.head = node;
+        } else {
+            node.prev = this.cur.prev;
+            this.cur.prev.next = node;
+            node.next = this.cur;
+            this.cur.prev = node;
+        }
+    }
+
+    addBack(node) {
+        if (this.cur.next === null) {
+            node.prev = this.cur;
+            this.cur.next = node;
+            this.tail = node;
+        } else {
+            node.next = this.cur.next;
+            node.prev = this.cur;
+            this.cur.next.prev = node;
+            this.cur.next = node;
+        }
+    }
+
+    moveFront() {
+        this.cur = this.cur.prev;
+    }
+
+    moveBack() {
+        this.cur = this.cur.next;
+    }
+
+    print() {
+        let prints = [];
+
+        if (this.cur.prev === null) {
+            prints.push("(Null)");
+        } else prints.push(this.cur.prev.data);
+
+        if (this.cur === null) {
+            prints.push("(Null)");
+        } else prints.push(this.cur.data);
+
+        if (this.cur.next === null) {
+            prints.push("(Null)");
+        } else prints.push(this.cur.next.data);
+
+        console.log(prints.join(" "));
+    }
+}
+
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const str = input[0].trim();
+const coms = input.slice(2).map(i => i.trim().split(" "));
+
+let dll = new DLL(str);
+
+coms.forEach(com => {
+    switch(Number(com[0])) {
+        case 1:
+            dll.addFront(new Node(com[1]));
+            break;
+        case 2:
+            dll.addBack(new Node(com[1]));
+            break;
+        case 3:
+            dll.moveFront();
+            break;
+        case 4:
+            dll.moveBack();
+            break;
+    }
+    dll.print();
+})
