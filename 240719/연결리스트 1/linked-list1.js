7016cad26fa73c8a8d9494848096edf6dcd1ff69
givemea1 <givemea4702@gmail.com>
2024-07-19 12:19:27 +0900

    Title: 연결리스트 1, Time: 3317ms, Memory: 105MB, Status: Passed - CodeTree

--- 240719/연결리스트 1/linked-list1.js
+++ 240719/연결리스트 1/linked-list1.js
@@ -6,64 +6,6 @@ class Node {
     }
 }
 
-class DLL {
-    constructor(str) {
-        let node = new Node(str);
-        this.cur = node;
-        this.size = 1;
-    }
-
-    addFront(node) {
-        if (this.cur.prev === null) {
-            node.next = this.cur;
-            this.cur.prev = node;
-        } else {
-            node.prev = this.cur.prev;
-            this.cur.prev.next = node;
-            node.next = this.cur;
-            this.cur.prev = node;
-        }
-    }
-
-    addBack(node) {
-        if (this.cur.next === null) {
-            node.prev = this.cur;
-            this.cur.next = node;
-        } else {
-            node.next = this.cur.next;
-            node.prev = this.cur;
-            this.cur.next.prev = node;
-            this.cur.next = node;
-        }
-    }
-
-    moveFront() {
-        if (this.cur.prev) this.cur = this.cur.prev;
-    }
-
-    moveBack() {
-        if (this.cur.next) this.cur = this.cur.next;
-    }
-
-    print() {
-        let prints = [];
-
-        if (this.cur.prev === null) {
-            prints.push("(Null)");
-        } else prints.push(this.cur.prev.data);
-
-        if (this.cur === null) {
-            prints.push("(Null)");
-        } else prints.push(this.cur.data);
-
-        if (this.cur.next === null) {
-            prints.push("(Null)");
-        } else prints.push(this.cur.next.data);
-
-        console.log(prints.join(" "));
-    }
-}
-
 const fs = require('fs');
 
 const input = fs.readFileSync(0).toString().trim().split("\n");
@@ -71,22 +13,56 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const str = input[0].trim();
 const coms = input.slice(2).map(i => i.trim().split(" "));
 
-let dll = new DLL(str);
+let cur = new Node(str);
+
+const insertPrev = (cur, node) => {
+    node.prev = cur.prev;
+    node.next = cur;
+
+    if (node.prev !== null) node.prev.next = node;
+    if (node.next !== null) node.next.prev = node;
+}
+
+const insertNext = (cur, node) => {
+    node.next = cur.next;
+    node.prev = cur;
+
+    if (node.next !== null) node.next.prev = node;
+    if (node.prev !== null) node.prev.next = node;
+}
+
+const print = (cur) => {
+    let prints = [];
+
+    if (cur.prev === null) {
+        prints.push("(Null)");
+    } else prints.push(cur.prev.data);
+
+    if (cur === null) {
+        prints.push("(Null)");
+    } else prints.push(cur.data);
+
+    if (cur.next === null) {
+        prints.push("(Null)");
+    } else prints.push(cur.next.data);
+
+    console.log(prints.join(" "));
+}
 
 coms.forEach(com => {
     switch(Number(com[0])) {
         case 1:
-            dll.addFront(new Node(com[1]));
+            insertPrev(cur, new Node(com[1]));
             break;
         case 2:
-            dll.addBack(new Node(com[1]));
+            insertNext(cur, new Node(com[1]));
             break;
         case 3:
-            dll.moveFront();
+            if (cur.prev) cur = cur.prev;
             break;
         case 4:
-            dll.moveBack();
+            if (cur.next) cur = cur.next;
             break;
     }
-    dll.print();
+    print(cur);
 })
