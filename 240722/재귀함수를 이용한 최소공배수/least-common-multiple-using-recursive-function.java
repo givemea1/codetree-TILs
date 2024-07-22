edb8b1ffd5fc7c5eb2271a7a29c7da94cefa8c8c
givemea1 <givemea4702@gmail.com>
2024-07-22 21:48:17 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        arr = new int[num];
+
+        for(int i = 0 ; i< num; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(f(num));
+    }
+
+    public static int f(int n){
+        if(n==1){
+            return gc(arr[n], arr[n-1]);
+        }
+        return gc(arr[n-1], f(n-1));
+    }
+
+    public static int gc(int a, int b){
+        for(int i = Math.max(a,b); i<=a*b; i++){
+            if(i % a == 0  && i % b == 0){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
