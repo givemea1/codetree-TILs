edd4b9a2f417ff40a9b174a33ecef7c47e51226d
givemea1 <givemea4702@gmail.com>
2024-07-22 17:33:11 +0900

    Title: 점 개수 세기 3, Time: 1379ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240722/점 개수 세기 3/count-number-of-points-3.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val nums =
+        br
+            .readLine()
+            .trim()
+            .split(" ")
+            .map { it.toInt() }
+    var treeSet = TreeSet<Int>()
+    var hashMap = HashMap<Int, Int>()
+    treeSet.addAll(
+        br
+            .readLine()
+            .trim()
+            .split(" ")
+            .map { it.toInt() }
+    )
+    for (i in treeSet.indices) {
+        hashMap.put(i, treeSet.first())
+        treeSet.remove(treeSet.first())
+    }
+    for (i in 0 until nums[1]) {
+        var start = -1
+        var end = -1
+        val n =
+            br
+                .readLine()
+                .trim()
+                .split(" ")
+                .map { it.toInt() }
+        for (j in 0 until hashMap.size) {
+            if (hashMap.get(j)!! <= n[0]) {
+                start = j
+            }
+            if (hashMap.get(j)!! <= n[1]) {
+                end = j
+            }
+        }
+        if (start == -1 && end == -1) {
+            println(0)
+        } else {
+            println(end - start + 1)
+        }
+    }
+}
