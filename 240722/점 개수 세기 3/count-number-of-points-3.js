e744ecf1ea1766f4a6803784107bb0b9b52cbc8c
givemea1 <givemea4702@gmail.com>
2024-07-22 22:13:28 +0900

    Title: 점 개수 세기 3, Time: 5028ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

--- 240722/점 개수 세기 3/count-number-of-points-3.js
+++ 240722/점 개수 세기 3/count-number-of-points-3.js
@@ -4,11 +4,18 @@ const [n, q] = input[0].trim().split(" ").map(Number);
 const points = input[1].trim().split(" ").map(Number);
 const sections = input.slice(2).map(i => i.trim().split(" ").map(Number));
 
-let map = new Map();
-points.forEach((val, idx) => {
-    map.set(val, idx + 1);
-})
+const BS = (points, val) => {
+    let i = Math.floor(points.length / 2);
+
+    while (true) {
+        if (points[i] === val) break;
+        if (val < points[i]) i = Math.floor(i / 2);
+        else i += Math.ceil(i / 2);
+    }
+
+    return i;
+}
 
 sections.forEach(s => {
-    console.log(map.get(s[1]) - map.get(s[0]) + 1);
+    console.log(BS(points, s[1]) - BS(points, s[0]) + 1);
 })
