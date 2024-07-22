d482423d38b8ce8aa7987c217c36ebd0e01642fd
givemea1 <givemea4702@gmail.com>
2024-07-22 17:53:33 +0900

    Title: 점 개수 세기 3, Time: 1488ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240722/점 개수 세기 3/count-number-of-points-3.kt
+++ 240722/점 개수 세기 3/count-number-of-points-3.kt
@@ -10,17 +10,16 @@ fun main() {
             .map { it.toInt() }
     var a = 0
     var hashMap = HashMap<Int, Int>()
-    val arr =
-        br
-            .readLine()
-            .trim()
-            .split(" ")
-            .map { it.toInt() }
-            .sorted()
-            .map {
-                hashMap.put(a, it)
-                a += 1
-            }
+    br
+        .readLine()
+        .trim()
+        .split(" ")
+        .map { it.toInt() }
+        .sorted()
+        .map {
+            hashMap.put(a, it)
+            a += 1
+        }
     for (i in 0 until nums[1]) {
         var start = -1
         var end = -1
@@ -33,15 +32,13 @@ fun main() {
         for (j in 0 until hashMap.size) {
             if (hashMap.get(j)!! <= n[0]) {
                 start = j
+                end = j
+                continue
             }
             if (hashMap.get(j)!! <= n[1]) {
                 end = j
             }
         }
-        if (start == -1 && end == -1) {
-            println(0)
-        } else {
-            println(end - start + 1)
-        }
+        println(end - start + 1)
     }
 }
