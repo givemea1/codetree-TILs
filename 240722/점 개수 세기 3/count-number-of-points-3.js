091fe98093b47430280621fe0d3fea90dc26343b
givemea1 <givemea4702@gmail.com>
2024-07-22 22:07:43 +0900

    Title: 점 개수 세기 3, Time: 1565ms, Memory: 58MB, Status: Passed - CodeTree

--- 240722/점 개수 세기 3/count-number-of-points-3.js
+++ 240722/점 개수 세기 3/count-number-of-points-3.js
@@ -4,6 +4,11 @@ const [n, q] = input[0].trim().split(" ").map(Number);
 const points = input[1].trim().split(" ").map(Number);
 const sections = input.slice(2).map(i => i.trim().split(" ").map(Number));
 
-for (let i = 0; i < q; i++) {
-    console.log(points.filter(p => p >= sections[i][0] && p <= sections[i][1]).length);
-}
+let map = new Map();
+points.forEach((val, idx) => {
+    map.set(val, idx + 1);
+})
+
+sections.forEach(s => {
+    console.log(map.get(s[1]) - map.get(s[0]) + 1);
+})
