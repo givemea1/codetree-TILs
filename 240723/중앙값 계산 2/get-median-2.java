79ce7b676b62f571e0a90ef18aacc195282797ec
givemea1 <givemea4702@gmail.com>
2024-07-23 01:09:20 +0900

    Title: 중앙값 계산 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/중앙값 계산 2/get-median-2.java
+++ 240723/중앙값 계산 2/get-median-2.java
@@ -12,12 +12,15 @@ public class Main {
 
         for(int i = 0; i < n ; i++){
             arr[i] = sc.nextInt();
+            count++;
             if(i == 0){
+                
                 System.out.print(arr[0]+" ");
                 continue;
             }
-            Arrays.sort(arr,0,count+1);
+        
             if(i % 2 == 0){
+                Arrays.sort(arr,0,count);
                 System.out.print(arr[(i+1)/2]+" ");
             }
         }
