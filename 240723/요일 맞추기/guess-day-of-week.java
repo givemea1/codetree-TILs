9993f780f42f240c0ad845af110fb3cb68df1962
givemea1 <givemea4702@gmail.com>
2024-07-23 14:48:04 +0900

    Title: 요일 맞추기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240723/요일 맞추기/guess-day-of-week.java
+++ 240723/요일 맞추기/guess-day-of-week.java
@@ -17,49 +17,78 @@ public class Main {
         int m2 ;
         int d2 ;
 
+        int count = 0;
+
         if((input_m1 < input_m2) ||(input_m1==input_m2 && input_d1<input_d2) ){
             m1 = input_m1;
             d1 = input_d1;
             m2 = input_m2;
             d2 = input_d2;
+            while(true){
+                if(m1 == m2 && d1 == d2){
+                    break;
+                }
+                count++;
+                if(d1 == days[m1]){
+                    m1++;
+                    d1 = 1;
+                }
+                d1++;
+            }
+            if(count % 7 == 0){
+                System.out.println("Mon");
+            }else if(count % 7 == 1){
+                System.out.println("Tue");
+            }
+            else if(count % 7 == 2){
+                System.out.println("Wen");
+            }
+            else if(count % 7 == 3){
+                System.out.println("Thu");
+            }
+            else if(count % 7 == 4){
+                System.out.println("Fri");
+            }
+            else if(count % 7 == 5){
+                System.out.println("Sat");
+            }else{
+                System.out.println("Sun");
+            }
         }else{
             m1 = input_m2;
             d1 = input_d2;
             m2 = input_m1;
             d2 = input_d1;  
-        }
-        int count = 0;
-
-        while(true){
-            if(m1 == m2 && d1 == d2){
-                break;
+            while(true){
+                if(m1 == m2 && d1 == d2){
+                    break;
+                }
+                count++;
+                if(d1 == days[m1]){
+                    m1++;
+                    d1 = 1;
+                }
+                d1++;
             }
-            count++;
-            if(d1 == days[m1]){
-                m1++;
-                d1 = 1;
+            if(count % 7 == 0){
+                System.out.println("Mon");
+            }else if(count % 7 == 1){
+                System.out.println("Sun");
             }
-            d1++;
+            else if(count % 7 == 2){
+                System.out.println("Sat");
+            }
+            else if(count % 7 == 3){
+                System.out.println("Fri");
+            }
+            else if(count % 7 == 4){
+                System.out.println("Thu");
+            }
+            else if(count % 7 == 5){
+                System.out.println("Wen");
+            }else{
+                System.out.println("Tue");
+            }
         }
-        if(count % 7 == 0){
-            System.out.println("Mon");
-        }else if(count % 7 == 1){
-            System.out.println("Tue");
-        }
-        else if(count % 7 == 2){
-            System.out.println("Wen");
-        }
-        else if(count % 7 == 3){
-            System.out.println("Thu");
-        }
-        else if(count % 7 == 4){
-            System.out.println("Fri");
-        }
-        else if(count % 7 == 5){
-            System.out.println("Sat");
-        }else{
-            System.out.println("Sun");
-        }
-        
     }
 }
