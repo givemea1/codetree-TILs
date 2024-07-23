adcc745ce14242c431c415031e24f8a86e428cf1
givemea1 <givemea4702@gmail.com>
2024-07-23 14:41:30 +0900

    Title: 요일 맞추기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240723/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] days = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input_m1 = sc.nextInt();
+        int input_d1 = sc.nextInt();
+        int input_m2 = sc.nextInt();
+        int input_d2 = sc.nextInt();
+
+        int m1 ;
+        int d1 ;
+        int m2 ;
+        int d2 ;
+
+        if((input_m1 < input_m2) ||(input_m1==input_m2 && input_d1<input_d2) ){
+            m1 = input_m1;
+            d1 = input_d1;
+            m2 = input_m2;
+            d2 = input_d2;
+        }else{
+            m1 = input_m2;
+            d1 = input_d2;
+            m2 = input_m1;
+            d2 = input_d1;  
+        }
+        int count = 0;
+
+        while(true){
+            if(m1 == m2 && d1 == d2){
+                break;
+            }
+            count++;
+            if(d1 == days[m1]){
+                m1++;
+                d1 = 1;
+            }
+            d1++;
+        }
+        if(count % 7 == 0){
+            System.out.println("Mon");
+        }else if(count % 7 == 1){
+            System.out.println("Tue");
+        }
+        else if(count % 7 == 2){
+            System.out.println("Wen");
+        }
+        else if(count % 7 == 3){
+            System.out.println("Thu");
+        }
+        else if(count % 7 == 4){
+            System.out.println("Fri");
+        }
+        else if(count % 7 == 5){
+            System.out.println("Sat");
+        }else{
+            System.out.println("Sun");
+        }
+        
+    }
+}
