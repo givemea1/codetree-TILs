d44ee6a6c24d7a6648ce5d103946a58d8467d0d1
givemea1 <givemea4702@gmail.com>
2024-07-24 17:03:54 +0900

    Title: 두 사람, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/두 사람/two-person.py
+++ 240724/두 사람/two-person.py
@@ -7,7 +7,7 @@ gen1 = a[1]
 age2 = int(b[0])
 gen2 = b[1]
 
-if((gen1=="M" and age1>=19) or (gen2=="M" and age2>=10) ):
+if((gen1=="M" and age1>=19) or (gen2=="M" and age2>=19) ):
     print(1)
 else:
     print(0)
