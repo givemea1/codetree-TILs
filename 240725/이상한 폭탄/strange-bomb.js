726e58b5f444a5d4b50c995facae1bb3f4b0a24b
givemea1 <givemea4702@gmail.com>
2024-07-25 03:27:53 +0900

    Title: 이상한 폭탄, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240725/이상한 폭탄/strange-bomb.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync(0).toString().trim().split("\n");
+
+const [n, k] = input[0].split(" ").map(Number);
+const bombs = input.slice(1).map(i => Number(i.trim()));
+
+let map = new Map();
+let max = 0;
+
+for (let i = 0; i < n; i++) {
+    if (!map.has(bombs[i])) map.set(bombs[i], i + k);
+    else {
+        if (i <= map.get(bombs[i])) max = Math.max(max, bombs[i]);
+    }
+}
+
+console.log(max);
