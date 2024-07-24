8a952f233a2abc37a4d91edf3b5ca042c030bfd8
givemea1 <givemea4702@gmail.com>
2024-07-24 16:28:41 +0900

    Title: 비교 연산, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/비교 연산/comparison-operator.py
@@ -0,0 +1,29 @@
+arr = input().split()
+
+n1 = int(arr[0])
+n2 = int(arr[1])
+
+if n1>n2:
+    print(1)
+else:
+    print(0)
+if n1>=n2:
+    print(1)
+else:
+    print(0)
+if n1<=n2:
+    print(1)
+else:
+    print(0)
+if n1<n2 : 
+    print(1)
+else:
+    print(0)
+if n1==n2 :
+    print(1)
+else:
+    print(0)
+if n1!=n2:
+    print(1)
+else:
+    print(0)
