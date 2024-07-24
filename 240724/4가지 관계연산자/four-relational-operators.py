b4facd2b196f9cc51b8699f87605163118b5e1a5
givemea1 <givemea4702@gmail.com>
2024-07-24 16:30:54 +0900

    Title: 4가지 관계연산자, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,24 @@
+arr = input().split()
+
+n1 = int(arr[0])
+n2 = int(arr[1])
+
+if n1>=n2:
+    print(1)
+else:
+    print(0)
+
+if n1>n2:
+    print(1)
+else:
+    print(0)
+
+if n2>=n1:
+    print(1)
+else:
+    print(0)
+
+if n2>n1:
+    print(1)
+else:
+    print(0)
