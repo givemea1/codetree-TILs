caa49b099d5c2e24d131311fdefa32741cddd0ee
givemea1 <givemea4702@gmail.com>
2024-07-24 16:52:38 +0900

    Title: 장학금, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240724/장학금/scholarship.py
+++ 240724/장학금/scholarship.py
@@ -3,9 +3,9 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-if a!=100 or b<90:
-    print(0)
-elif b>=95:
+if a==100 and b>=95:
     print(100000)
-else:
+elif a==100 and b>=90:
     print(50000)
+else:
+    print(0)
