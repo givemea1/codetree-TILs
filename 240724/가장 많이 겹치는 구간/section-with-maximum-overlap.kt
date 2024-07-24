a1677db92ca7d025aafc44772b34ec8d8374e603
givemea1 <givemea4702@gmail.com>
2024-07-24 16:32:14 +0900

    Title: 가장 많이 겹치는 구간, Time: 147ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240724/가장 많이 겹치는 구간/section-with-maximum-overlap.kt
@@ -0,0 +1,41 @@
+import java.io.*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val nums =
+        br
+            .readLine()
+            .toInt()
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    var arr = IntArray(200000)
+    for (i in 0 until nums) {
+        val n =
+            br
+                .readLine()
+                .trim()
+                .split(" ")
+                .map { it.toInt() }
+                .toIntArray()
+        if (max < n[1]) {
+            max = n[1]
+        }
+        if (min > n[0]) {
+            min = n[0]
+        }
+        arr[n[0]] = 1
+        arr[n[1]] = -1
+    }
+    var count = 0
+    var maxCount = 0
+    var now = 0
+    for (i in min..max) {
+        now += arr[i]
+        if (now > maxCount) {
+            count = 1
+            maxCount = now
+        } else if (arr[i] != 0 && now == maxCount) {
+            count++
+        }
+    }
+    println(count)
+}
