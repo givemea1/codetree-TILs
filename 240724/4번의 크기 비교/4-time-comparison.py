6eed109651a53f89a2884399538f0a4d23ab4b8f
givemea1 <givemea4702@gmail.com>
2024-07-24 16:32:30 +0900

    Title: 4번의 크기 비교, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+arr = input().split()
+n1 = int(arr[0])
+n2 = int(arr[1])
+n3 = int(arr[2])
+n4 = int(arr[3])
+
+print(int(a>n1))
+print(int(a>n2))
+print(int(a>n3))
+print(int(a>n4))
