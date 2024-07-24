7246421608e4826197b0b3e7f1901efe722a42ca
givemea1 <givemea4702@gmail.com>
2024-07-25 04:17:03 +0900

    Title: 가장 많이 겹치는 구간, Time: 5081ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

--- 240725/가장 많이 겹치는 구간/section-with-maximum-overlap.js
+++ 240725/가장 많이 겹치는 구간/section-with-maximum-overlap.js
@@ -5,14 +5,11 @@ const n = Number(input[0].trim());
 const sections = input.slice(1).map(i => i.trim().split(' ').map(Number));
 
 const nums = sections.flat().sort((a, b) => a - b);
-let sectionCount = Array(nums.length - 1).fill(0); // sectionCount[i] = 구간 [nums[i], nums[i + 1]] 횟수
+let sectionCount = Array(n * 2 - 1).fill(0); // sectionCount[i] = 구간 [nums[i], nums[i + 1]] 횟수
 let max = 0;
 
 for (let section of sections) {
-    let s = section[0];
-    let e = section[1];
-
-    for (let i = nums.indexOf(s); i < nums.indexOf(e); i++) {
+    for (let i = nums.indexOf(section[0]); i < nums.indexOf(section[1]); i++) {
         max = Math.max(max, ++sectionCount[i]);
     }
 }
