c33de8aae4817322d295f7bbb0cd10dadcd94c87
givemea1 <givemea4702@gmail.com>
2024-07-26 09:35:46 +0900

    Title: 369 게임, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240726/369 게임/369-game.py
+++ 240726/369 게임/369-game.py
@@ -1,9 +1,9 @@
 n = int(input())
 
 for i in range(1,n+1):
-    if i % 10 == 3 or i%10==6 or i % 10==9:
+    if i % 10 == 3 or i%10 == 6 or i % 10 == 9:
         print(0, end=" ")
-    elif i // 10 == 3 or i // 10 == 6 or i//10==9:
+    elif i // 10 == 3 or i // 10 == 6 or i//10 == 9:
         print(0, end=" ")
     else:
         print(i,end=" ")
