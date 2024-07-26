0690ec81d338cb84bb2d872b7d644a172c02085b
givemea1 <givemea4702@gmail.com>
2024-07-26 21:05:29 +0900

    Title: 가장 짧은 부분합, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240726/가장 짧은 부분합/shortest-subtotal.js
+++ 240726/가장 짧은 부분합/shortest-subtotal.js
@@ -1,20 +1,35 @@
-const input = require('fs').readFileSync(0).toString().trim().split("\n");
+const main = () => {
+    const input = require('fs').readFileSync(0).toString().trim().split("\n");
 
-const [n, s] = input[0].trim().split(" ").map(Number);
-const nums = input[1].trim().split(" ").map(Number);
+    const [n, s] = input[0].trim().split(" ").map(Number);
+    const nums = input[1].trim().split(" ").map(Number);
 
-let min = n;
-
-for (let i = 0; i < n; i++) {
-    let sum = 0;
-    let limit = i + min > n ? n : i + min
-    for (let j = i; j < limit; j++) {
-        sum += nums[j];
-        if (sum >= s) {
-            min = Math.min(min, j - i + 1);
-            break;
-        }
-    }
+    console.log(search(n, s, nums));
 }
 
-console.log(min);
+const search = (n, s, nums) => {
+    let sum = 0;
+    let l = 0;
+    let r = 0;
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+        if (sum >= s) r = i;
+    }
+
+    let min = r - l + 1;
+
+    while (r < n) {
+        while (sum >= s) {
+            l++;
+            sum -= nums[l];
+        }
+        
+        min = r - l + 1;
+        r++;
+        sum += nums[r];
+    }
+
+    return min;
+}
+
+main();
