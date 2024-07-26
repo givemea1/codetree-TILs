9672dfdffa40ce2b34330d4e62c118313f49379c
givemea1 <givemea4702@gmail.com>
2024-07-26 20:31:47 +0900

    Title: 가장 짧은 부분합, Time: 5077ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 240726/가장 짧은 부분합/shortest-subtotal.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync(0).toString().trim().split("\n");
+
+const [n, s] = input[0].trim().split(" ").map(Number);
+const nums = input[1].trim().split(" ").map(Number);
+
+let min = n;
+
+for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = i; j < n; j++) {
+        sum += nums[j];
+        if (sum >= s) {
+            min = Math.min(min, j - i + 1);
+        }
+    }
+}
+
+console.log(min);
