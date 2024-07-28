4866775e3c34c073fda453915e2cb2e36fdfd20d
givemea1 <givemea4702@gmail.com>
2024-07-28 20:52:09 +0900

    Title: 가장 짧은 부분합, Time: 318ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240728/가장 짧은 부분합/shortest-subtotal.kt
@@ -0,0 +1,42 @@
+import java.io.*
+import kotlin.math.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val nums =
+        br
+            .readLine()
+            .trim()
+            .split(" ")
+            .map { it.toInt() }
+            .toTypedArray()
+    val arr =
+        br
+            .readLine()
+            .trim()
+            .split(" ")
+            .map { it.toInt() }
+            .toTypedArray()
+    var min = Int.MAX_VALUE
+    var p1 = 0
+    var p2 = 0
+    var total = arr[0]
+    var count = 1
+    while (p1 < nums[0] && p2 < nums[0]) {
+        if (total >= nums[1]) {
+            min = min(min, count)
+            total -= arr[p1]
+            p1++
+            count--
+        }
+        if (p1 == p2 || total < nums[1]) {
+            p2++
+            if (p2 == nums[0]) {
+                break
+            }
+            total += arr[p2]
+            count++
+        }
+    }
+    println(if (min == Int.MAX_VALUE) 0 else min)
+}
