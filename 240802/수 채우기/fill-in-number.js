9e4f12a238ec3ea2c4548886cb93668e564229ac
givemea1 <givemea4702@gmail.com>
2024-08-02 22:22:03 +0900

    Title: 수 채우기, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240802/수 채우기/fill-in-number.js
+++ 240802/수 채우기/fill-in-number.js
@@ -1,8 +1,17 @@
 const main = () => {
     const input = Number(require('fs').readFileSync(0).toString().trim());
-    let answer = Math.floor(input / 5) + (input % 5) / 2
-    
-    console.log(input % 5 % 2 ? -1 : answer);
+    console.log(getCount(input));
+}
+
+const getCount = (val) => {
+    let s5 = Math.floor(val / 5);
+    let rest = val % 5;
+
+    if ((val % 5) % 2) {
+        if (s5 === 0) return -1;
+        return s5 - 1 + (rest + 5) / 2;
+    }
+    return s5 + rest % 2;
 }
 
 main();
