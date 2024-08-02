60bf2cf91c7381234a0c5e833b54d7ce5306ef7f
givemea1 <givemea4702@gmail.com>
2024-08-02 16:37:08 +0900

    Title: 겹치지 않게 선분 고르기, Time: 145ms, Memory: 9MB, Status: Passed - CodeTree

--- 240802/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240802/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -44,7 +44,7 @@ public class Main {
                 int[] select1 =selected.get(i);
                 int[] select2 = selected.get(j);
                 if((select1[0] < select2[0] && select1[1] >= select2[0]) || 
-                (select2[0] < select1[0] && select2[1] >= select1[1])){
+                (select2[0] < select1[0] && select2[1] >= select1[0])){
                     return false;
                 }
             }
