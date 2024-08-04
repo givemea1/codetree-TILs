55dbf9ffaa76ccbf7b71b4eeafd02d286984f187
givemea1 <givemea4702@gmail.com>
2024-08-04 23:32:45 +0900

    Title: 자연수 n개의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/자연수 n개의 합/sum-of-n-natural-numbers.kt
@@ -0,0 +1,20 @@
+import java.io.*
+import kotlin.math.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val num = br.readLine().toInt()
+    var right = (sqrt(num.toDouble()).toInt() * 2)
+    var left = 0
+    var result =-1
+    while (left<right) {
+        val mid = (right + left) / 2
+        if ((mid * (mid - 1)) / 2 < num) {
+            left = mid+1
+            result = mid
+        }else{
+            right = mid-1
+        }
+    }
+    println(result)
+}
