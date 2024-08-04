23fdc9dabdaeb4d7c5c57322256deed0ff390050
givemea1 <givemea4702@gmail.com>
2024-08-04 23:42:33 +0900

    Title: 자연수 n개의 합, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

--- 240804/자연수 n개의 합/sum-of-n-natural-numbers.kt
+++ 240804/자연수 n개의 합/sum-of-n-natural-numbers.kt
@@ -1,17 +1,18 @@
 import java.io.*
 import kotlin.math.*
 
+
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val num = br.readLine().toLong()
     var right = (sqrt(num.toDouble()).toLong() * 2)
     var left = 0L
-    var result = -1L
-    while (left < right) {
+    var result = 0L
+    while (left <= right) {
         val mid = (right + left) / 2
         if ((mid * (mid + 1)) / 2 <= num) {
             left = mid + 1
-            result = mid
+            result = max(mid, result)
         } else {
             right = mid - 1
         }
